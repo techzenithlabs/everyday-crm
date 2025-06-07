b0383222e5dc7e1555be4753cb971fe232752437
techzenithlabs <techzenithlabs@gmail.com>
2025-06-07 21:27:20 +0530

    run migrations and seed

---
+++ server/database/seeders/AdminUserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'email_verified_at' => Carbon::now(),
+                'password' => Hash::make('admin@123'),
+                'role_id' => 1,
+                'status' => 1,
+                'remember_token' => null,
+            ]
+        );
+    }
+}
