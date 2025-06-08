46ab9cfcc0b56f69a6bfdf5f0cb57e1c68b56257
techzenithlabs <techzenithlabs@gmail.com>
2025-06-08 15:36:11 +0530

    stored redux for storage

--- server/app/Http/Middleware/CorsMiddleware.php
+++ server/app/Http/Middleware/CorsMiddleware.php
@@ -8,19 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
-         return $response->withHeaders([
-            'Access-Control-Allow-Origin' => 'http://localhost:5173',
-            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization, X-Requested-With',
-            'Access-Control-Allow-Credentials' => 'true',
-        ]);
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
     }
 }
